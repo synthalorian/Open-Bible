690f0ce38eb8d8bc251056f64a3f6c8232b3b2a5
blackclaw <synthalorian@gmail.com>
2026-07-23 19:05:22 -0400

    fix(genealogy): complete Luke lineage and immediate family lookup

--- test/features/genealogy_test.dart
+++ test/features/genealogy_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:open_bible/features/genealogy/services/genealogy_service.dart';
 import 'package:flutter/services.dart';
-import 'dart:convert';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
